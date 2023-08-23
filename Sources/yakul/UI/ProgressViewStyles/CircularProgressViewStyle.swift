93beba9ea7e91cd20e6550552e6854031105ef85
Galen <galenqq@gmail.com>
2023-08-23 15:23:06 +1000

    added progress view style

---
+++ Sources/yakul/UI/ProgressViewStyles/CircularProgressViewStyle.swift
@@ -0,0 +1,34 @@
+import SwiftUI
+
+@available(iOS 15.0, *)
+public struct CircularProgressViewStyle: ProgressViewStyle {
+    
+    @State private var isLoading = false
+    
+    private var gradient: AngularGradient {
+        .init(
+            gradient: Gradient(colors: [.black, .black.opacity(0)]),
+            center: .center,
+            startAngle: .degrees(270),
+            endAngle: .degrees(0)
+        )
+    }
+
+    public func makeBody(configuration: Configuration) -> some View {
+        return ZStack {
+            Circle()
+                .stroke(gradient, style: StrokeStyle(lineWidth: 5, lineCap: .round))
+                .frame(maxWidth: 75, maxHeight: 75)
+                .rotationEffect(.degrees(isLoading ? 360 : 0))
+                .onAppear {
+                    withAnimation(
+                        Animation
+                            .linear(duration: 1)
+                            .repeatForever(autoreverses: false)
+                    ) {
+                        isLoading.toggle()
+                    }
+                }
+        }
+    }
+}
